804b10587e03448d72bd48e937ec458819ff558f
Marcin Wojciechowski <m.wojciechowski.653@studms.ug.edu.pl>
2024-01-10 22:17:02 +0100

    Endpoint /tmdb/movies/popular

--- routes/tmdb.js
+++ routes/tmdb.js
@@ -5,7 +5,19 @@ const dbo = require("../db/conn");
 
 recordRoutes.get("/tmdb", async function(req, res) {
     
-    let result = await dbo.getDB().collection("TMDB").find({}).limit(20).toArray();
+    let result = await dbo.getDB().collection("TMDB").find({}).limit(5).toArray();
+
+    res.status(200).send(result);
+})
+
+recordRoutes.get("/tmdb/movies/popular", async function(req, res) {
+    
+    let result = await dbo.getDB().collection("TMDB").aggregate([
+        { $project: { _id:0, title:1, languages:1, genres:1, plot:1, 
+            popularity: {$round: [{$multiply: [ {$toDouble: "$imdb.rating"}, {$toInt: "$imdb.votes"} ]}, 0]} }},
+        { $sort: {popularity: -1}},
+        { $limit: 5}
+        ]).toArray();
 
     res.status(200).send(result);
 })
