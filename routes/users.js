a93dfdfd1c401bca97d7737bb2e4666bbf033c29
Marcin Wojciechowski <m.wojciechowski.653@studms.ug.edu.pl>
2024-01-10 13:52:12 +0100

    Dodanie endpointu /users/register

--- routes/users.js
+++ routes/users.js
@@ -3,9 +3,25 @@ const recordRoutes = express.Router();
 const ObjectId = require("mongodb").ObjectId;
 const dbo = require("../db/conn")
 
-recordRoutes.get("/Users", async function(req, res) {
-    let result = await dbo.getDB().collection("Users").findOne({name:"Marcin"})
-    res.send(result)
+recordRoutes.post("/users/register", async function(req, res) {
+
+    const user = {
+        login: req.body.login,
+        password: req.body.password,
+        sex: req.body.sex,
+        mail: req.body.mail,
+        phone: req.body.phone,
+        birth: new Date(req.params.birth)
+    };
+    const myQuery = {login: req.body.login};
+
+    let result = await dbo.getDB().collection("Users").findOne(myQuery);
+    if (result == undefined){
+        await dbo.getDB().collection("Users").insertOne(user);
+        res.status(200).send("You succesfully registered new acount!");
+    }else {
+        res.status(400).send("This login is already taken");
+    }
 });
 
 module.exports = recordRoutes;
