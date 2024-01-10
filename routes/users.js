e1f73572454fa355278d6d690ad7c10d17a342d6
Marcin Wojciechowski <m.wojciechowski.653@studms.ug.edu.pl>
2024-01-10 13:59:07 +0100

    Wyświetlanie wszystkich użytkowników + logowanie

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,13 @@ const recordRoutes = express.Router();
 const ObjectId = require("mongodb").ObjectId;
 const dbo = require("../db/conn")
 
+recordRoutes.get("/users", async function(req, res) {
+    
+    let result = await dbo.getDB().collection("Users").find({}).toArray();
+
+    res.status(200).send(result);
+})
+
 recordRoutes.post("/users/register", async function(req, res) {
 
     const user = {
@@ -21,6 +28,19 @@ recordRoutes.post("/users/register", async function(req, res) {
         res.status(200).send("You succesfully registered new acount!");
     }else {
         res.status(400).send("This login is already taken");
+    }
+});
+
+recordRoutes.post("/users/login", async function(req, res) {
+
+    const myQuery = {login: req.body.login, password: req.body.password};
+
+    let result = await dbo.getDB().collection("Users").findOne(myQuery);
+
+    if (result == undefined){
+        res.status(400).send("Login or/and password are incorrect")
+    }else {
+        res.status(200).send("You succesfully signed to your account!");
     }
 });
 
