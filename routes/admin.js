47fda8f4f32387a632465856b79c61c08fc47e94
Marcin Wojciechowski <m.wojciechowski.653@studms.ug.edu.pl>
2024-01-12 22:47:29 +0100

    Zmienianie statusu użytkownika przez admina

--- routes/admin.js
+++ routes/admin.js
@@ -9,18 +9,57 @@ recordRoutes.get("/admin/users", async function(req, res) {
         login: req.body.login,
         password: req.body.password,
         status: "Admin"
-    }
+    };
 
     const dataCheck = await dbo.getDB().collection("Users").find(myQuery).toArray();
 
     if (dataCheck.length === 0){
         res.status(403).send("Access denied");
         return
-    }
+    };
 
     const result = await dbo.getDB().collection("Users").find({}).toArray();
 
     res.status(200).send(result);
+});
+
+recordRoutes.put("/admin/users/:userId", async function(req, res) {
+
+    const userId = req.params.userId;
+
+    const myQuery = {
+        login: req.body.login,
+        password: req.body.password,
+        status: "Admin"
+    };
+
+    const dataCheck = await dbo.getDB().collection("Users").find(myQuery).toArray();
+
+    if (dataCheck.length === 0){
+        res.status(403).send("Access denied");
+        return
+    };
+
+    const myQuery2 = {
+        _id: new ObjectId(userId)
+    };
+
+    const dataCheckVol2 = await dbo.getDB().collection("Users").find(myQuery2).toArray();
+
+    if (dataCheckVol2.length === 0){
+        res.status(404).send("User not found");
+        return
+    };  
+
+    const myUpdate = {
+        $set: {status: req.body.newStatus}
+    };
+
+    dbo.getDB().collection("Users").updateOne(myQuery2, myUpdate)
+    .then(result => {
+        res.status(200).send("Status of this user has changed");
+    })
+    .catch(err => res.status(418).send(err));
 });
 
 module.exports = recordRoutes;
